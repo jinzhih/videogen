927f2489be62383f9b3a22cd1940816e893a229e
jinzhih <jessie.houjinzhi@gmail.com>
2024-03-23 14:54:31 +1100

    get data

--- controllers/video_controller.js
+++ controllers/video_controller.js
@@ -1,1 +1,27 @@
-import fs from "fs";
+import mysql from "mysql2/promise";
+
+const db = await mysql.createConnection({
+  host: "3.27.125.190",
+  port: "3306",
+  user: "admin",
+  password: "entertainment_admin",
+  database: "entertainment_db",
+});
+
+export async function GenerateVideo(request, response) {
+  const contentId = request.params.id;
+  try {
+    const [results, fields] = await db.query(
+      "SELECT * FROM content WHERE id =?",
+      [contentId]
+    );
+    const { chat_story } = results[0];
+    console.log(chat_story);
+    return response.json(results);
+  } catch (err) {
+    if (err != null) response.status(500).send({ err: err.message });
+  }
+
+  //Format data
+  //Put text on then generate video ; can refer imgPrep from old project
+}
--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 import express from "express";
-import mysql from "mysql2";
+import { GenerateVideo } from "./controllers/video_controller.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const db = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "root",
-  database: "env",
-});
 
 app.get("/", (req, res) => {
   res.json("hello this is the video gen root route");
 });
 
-app.get("/chapters", (req, res) => {
-  const q = "SELECT * FROM chapter";
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-});
+app.post("/generateVideo/:id", GenerateVideo);
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
